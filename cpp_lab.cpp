64d9efab906db9962adc091dc3d07dd245163d44
yuyue <yue.yu@semidrive.com>
2022-03-22 15:38:02 +0800

    Print C++ and Boost info

--- cpp_lab.cpp
+++ cpp_lab.cpp
@@ -2,8 +2,14 @@
 
 using namespace std;
 
+#include <boost/version.hpp>
+
 int main(int argc, char **argv)
 {
     cout << "Hello C++ lab" << endl;
+
+    cout << "C++ std: " << __cplusplus << endl;
+    cout << "boost version: " << BOOST_LIB_VERSION << endl;
+
     return 0;
 }
