a59abc60815101af024f4e93823f98324862f36c
yuyue <yue.yu@semidrive.com>
2022-03-24 15:14:11 +0800

    time format lab

--- cpp_lab.cpp
+++ cpp_lab.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 #include <boost/version.hpp>
+#include <boost/format.hpp>
+
+#include <sys/time.h>
 
 int main(int argc, char **argv)
 {
@@ -11,5 +14,11 @@ int main(int argc, char **argv)
     cout << "C++ std: " << __cplusplus << endl;
     cout << "boost version: " << BOOST_LIB_VERSION << endl;
 
+    struct timeval tv;
+    gettimeofday(&tv, nullptr);
+
+    struct tm *tm = localtime(&(tv.tv_sec));
+
+    cout << (boost::format("%02d:%02d:%02d.%03d") % tm->tm_hour % tm->tm_min % tm->tm_sec % (tv.tv_usec / 1000)).str() << endl;
     return 0;
 }
