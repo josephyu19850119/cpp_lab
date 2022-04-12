8ccf14a6e893e9206c70ede6ac3926aa5ac4028c
Yu Yue <yue.yu@semidrive.com>
2022-04-12 13:48:10 +0800

    timer by thread usleep

--- test.cpp
+++ test.cpp
@@ -1,4 +1,3 @@
-#include <chrono>
 #include <thread>
 using namespace std;
 
@@ -10,7 +9,7 @@ int main(int argc, char **argv)
 {
     int count = 0;
     // 每1000微秒(0.001秒)回调一次，1000次后取消
-    timer::timer_handle t = timer::set(chrono::duration<int, micro>(1000), &count, [](void *p) { 
+    timer::timer_handle t = timer::set(1000000, &count, [](void *p) { 
         int* count = static_cast<int*>(p);
         BOOST_LOG_TRIVIAL(info) << "tick tock: " << ++(*count);
         return true; });
@@ -23,7 +22,7 @@ int main(int argc, char **argv)
     timer::cancel(t);
 
     // 每1000微秒(0.001秒)回调一次，count降为0时主动停止
-    t = timer::set(chrono::duration<int, micro>(1000), &count, [](void *p) {
+    t = timer::set(1000000, &count, [](void *p) {
         int* count = static_cast<int*>(p);
         BOOST_LOG_TRIVIAL(info) << "tick tock: " << (*count)--;
         if (*count == 0) {
--- timer.hpp
+++ timer.hpp
@@ -5,6 +5,8 @@ _Pragma("once");
 #include <atomic>
 #include <functional>
 
+#include <unistd.h>
+
 class timer
 {
     class timer_context
@@ -17,15 +19,15 @@ public:
     using timer_handle = timer_context *;
 
     // 可以通过让TIMER_CALLBACK返回false,取消当前timer
-    typedef std::function<bool(void *)> TIMER_CALLBACK;
+    using TIMER_CALLBACK = std::function<bool(void *)>;
 
-    static timer_handle set(std::chrono::duration<int, std::micro> interval, void *user_data, TIMER_CALLBACK callback)
+    static timer_handle set(useconds_t  micro_seconds, void *user_data, TIMER_CALLBACK callback)
     {
         timer_handle t = new timer_context();
         std::thread([=] { 
                         while(!t->canceled) 
                         {
-                            std::this_thread::sleep_for(interval);
+                            usleep(micro_seconds);
                             if (callback(user_data) == false)
                             {
                                 break;
