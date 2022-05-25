955f93df7d7da348898005fdeff4ca134df8693e
Yu Yue <yue.yu@semidrive.com>
2022-05-25 17:46:07 +0800

    High priority task vs. low priority task.

--- cpp_lab.cpp
+++ cpp_lab.cpp
@@ -1,20 +1,69 @@
 #include "unios_task.h"
 
+#include <pthread.h>
+
 #include <iostream>
-#include <string>
+#include <atomic>
 using namespace std;
 
-#include <unistd.h>
+atomic_int n{1};
+pthread_barrier_t barrier;
+const int iteration_count = 100000000;
 
 int main(int argc, char **argv)
 {
-    UniOS::Task::Launch([](const string& str){
-        UniOS::Task::SubTask([](){
-            cout << "in sub Task" << endl;
-        });
-        cout  << str << endl;
-    }, UniOS::Task::TaskPriority::P10, false, "yuyue");
+    if (pthread_barrier_init(&barrier, nullptr, 2) != 0)
+    {
+        cerr << "pthread_barrier_init FAILED" << endl;
+        exit(-1);
+    }
 
-    sleep(5);
+    optional<thread> lowPriorityTask = UniOS::Task::Launch([]()
+    {
+        // 确保lowPriorityTask和highPriorityTask在开始执行测试代码前，都进入可调度状态
+        int ret = pthread_barrier_wait(&barrier);
+        if (ret != PTHREAD_BARRIER_SERIAL_THREAD && ret != 0)
+        {
+            cerr << "pthread_barrier_wait FAILED" << endl;
+            exit(-1);
+        }
+
+        for (int i = 0; i < iteration_count; ++i)
+        {
+            ++n;
+        }
+    },  UniOS::Task::TaskPriority::P0, false,
+        UniOS::Task::CpuSet("00000001")// 确保lowPriorityTask和highPriorityTask运行在同一个CPU上以验证效果
+    );
+
+    optional<thread> highPriorityTask = UniOS::Task::Launch([]()
+    {
+        // 确保lowPriorityTask和highPriorityTask在开始执行测试代码前，都进入可调度状态
+        int ret = pthread_barrier_wait(&barrier);
+        if (ret != PTHREAD_BARRIER_SERIAL_THREAD && ret != 0)
+        {
+            cerr << "pthread_barrier_wait FAILED" << endl;
+            exit(-1);
+        }
+
+        for (int i = 0; i < iteration_count - 1; ++i)
+        {
+            n = -n;
+        }
+    }, UniOS::Task::TaskPriority::P1, false,
+        UniOS::Task::CpuSet("00000001") // 确保lowPriorityTask和highPriorityTask运行在同一个CPU上以验证效果
+    );
+
+    lowPriorityTask.value().join();
+    highPriorityTask.value().join();
+
+    if (pthread_barrier_destroy(&barrier) != 0)
+    {
+        cerr << "pthread_barrier_destroy FAILED" << endl;
+        exit(-1);
+    }
+
+    // Expected output 99999999
+    cout << n << endl;
     return 0;
 }
