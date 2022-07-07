1fa8cbfc6ce5d1ef0477c0d5b7bb3982f58c7df2
Yu Yue <yue.yu@semidrive.com>
2022-07-07 16:20:17 +0800

    full cleaner.

--- cleaner.cpp
+++ cleaner.cpp
@@ -1,11 +1,46 @@
-#include "message_channel.hpp"
+#include "test.h"
 
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-    cout << boolalpha << boost::interprocess::shared_memory_object::remove(domain_shared_memory_name_formatter(1024).c_str()) << endl;
-    
+    if (argc < 2)
+    {
+        cout << "Usage: msg_chan_cleaner [domain id] [channel name]" << endl;
+        return 0;
+    }
+
+    int domain_id = stoi(argv[1]);
+    string channel_name;
+    if (argc > 2)
+    {
+        channel_name = argv[2];
+    }
+
+    if (monitoring_channel::remove(domain_id, channel_name))
+    {
+        if (channel_name.empty())
+        {
+            cout << "Removed shared memory of domain: " << domain_id << endl;
+        }
+        else
+        {
+            cout << "Removed channel: \"" << channel_name << "\" from shared memory of domain: " << domain_id << endl;
+        }
+    }
+    else
+    {
+        if (channel_name.empty())
+        {
+            cout << "Cannot find shared memory of domain: " << domain_id << endl;
+        }
+        else
+        {
+            cout << "Cannot find channel: \"" << channel_name << "\" from shared memory of domain: " << domain_id << endl;
+        }
+    }
+
     return 0;
 }
